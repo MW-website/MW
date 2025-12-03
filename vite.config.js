2872224e2f3121f90b47607dacff8a820ddd8b1e
Anon <Fortunestephen720@gmail.com>
2025-12-03 21:47:12 +0100

    build

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/MW/",
   plugins: [
     react(),
     tailwindcss()
